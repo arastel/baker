17f3d4f0018ad13339a05ab7915c0cbe792b2f4d
Alexandre RASTEL <alexandre.rastel@gmail.com>
2017-12-21 22:25:26 +0100

    Create toggle_desc.js

---
+++ js/toggle_desc.js
@@ -0,0 +1,27 @@
+function expand_dps()
+{
+    $('.plus_dps').removeClass('hidden');
+    $('.plus_nautique').addClass('hidden');
+    $('.plus_ps').addClass('hidden');
+}
+
+function expand_nautique()
+{
+    $('.plus_dps').addClass('hidden');
+    $('.plus_nautique').removeClass('hidden');
+    $('.plus_ps').addClass('hidden');
+}
+
+function expand_ps()
+{
+    $('.plus_dps').addClass('hidden');
+    $('.plus_nautique').addClass('hidden');
+    $('.plus_ps').removeClass('hidden');
+}
+
+function hide_plus()
+{
+    $('.plus_dps').addClass('hidden');
+    $('.plus_nautique').addClass('hidden');
+    $('.plus_ps').addClass('hidden');
+}
